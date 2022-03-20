2eabaab6b517f79102e56ba1f5156b4990647d67
Rutvi Shah <rutvishah859@gmail.com>
2022-03-20 18:35:17 -0400

    Did the sub-grid validator thread function
    Co-Authored-By: Foram <24356597+foramgandhi12@users.noreply.github.com>

--- sudoku_sol_validator.c
+++ sudoku_sol_validator.c
@@ -5,7 +5,7 @@
 #include <stdbool.h>
 
 /* structure for passing data to threads */ 
-typedef struct  
+typedef struct 
 {  
     int row;  
     int column;  
@@ -28,17 +28,18 @@ void *checkColumn(void *param) {
 	
 	for(int i = 0; i < 9; i++){
 		if(cols[board[i][col]] > 0){
-			cols[board[i][col]] += 1;
+			cols[board[i][col]]++;
 		}
 	}
 	
-	for(int i = 0; i < 9; i++){
+	for(int i = 1; i < 10; i++){
 		if(cols[i] > 1){
 			isValid = 0;
 			break;
 		}
 	}
 	validCol[col] = isValid;
+  pthread_exit(0);
 }
 
 // check each row of the sudoku board, validate of all the values in the row are unique [1, 9]
@@ -47,8 +48,35 @@ void *checkRow(void *param) {
 }
 
 // check each individual 3-by-3 grid, validate of all the values in the grid are unique [1, 9]
-void checkGrid(void *param) {
+void *checkGrid(void* param) {
+  parameters *p = (parameters*)param;
+  int isValid = 1;
 
+  // array holding num of occurrences of each value from 1-9 (ignore index 0)
+  int boxes[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  // get the sub-grid (row and col)
+  int rowBox =  (int)(p->row / 3) * 3;
+  int colBox = (int)(p->column / 3) * 3;
+
+  // check the sub-grid and update the num of occurrences of each value (from 0-9)
+  for (int i = rowBox; i < rowBox + 3; i++) {
+    for (int j = colBox; j < colBox + 3; j++) {
+      if (board[i][j] > 0) {
+        boxes[board[i][j]]++; 
+      }
+    }
+  }
+
+  // if there is more than 1 occurrence of a num the grid is invalid
+  for(int i = 1; i < 10; i++){
+    if(boxes[i] > 1){
+      isValid = 0;
+      break;
+    }
+  }
+  // update the validGrid 
+  validGrid[3*rowBox + colBox] = isValid;
+  pthread_exit(0);
 }
 
 // prints formatted sukoku board
@@ -65,7 +93,7 @@ void printBoard () {
       }
     }
     if (i == 2 || i == 5) {
-      printf("------------------\n");
+      printf("---------------------\n");
     }
   }
 }
@@ -101,7 +129,7 @@ int main(void) {
   int puzzleRow; 
   int puzzleCol;
   
-  pthread_t colVal;
+  pthread_t tid;
   
   // until the puzzle has not been fully solved (vaid or invalid) don't break the loop
   while (1) {
@@ -113,16 +141,20 @@ int main(void) {
     printf("Enter the column you'd like to fill: ");
     scanf("%d", &puzzleCol);
     
-    
-
     // position is invalid if it's already been set (position not equal to 0)
     if (board[puzzleCol][puzzleRow] != 0) {
       printf("Enter a position that is not already set\n");
       continue;
     }
     
-    pthread_create(&colVal, NULL, &checkColumn, &puzzleCol);
-    pthread_join(colVal, NULL);
+    // parameters *data = (parameters *) malloc(sizeof(parameters));
+    // data->row = puzzleRow;
+    // data->column = puzzleCol;
+
+    // pthread_create(&tid, NULL, checkGrid, data);
+    // pthread_join(tid, NULL);
+    // pthread_create(&tid, NULL, &checkColumn, &puzzleCol);
+    // pthread_join(colVal, NULL);
     
     //printf("%d %d\n", validCol[puzzleCol], validCol[puzzleCol+1]);
   }
