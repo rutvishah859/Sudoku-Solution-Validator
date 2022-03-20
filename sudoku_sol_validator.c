49af50cbd79dd2350b23506c7e1226e6d31a0af4
Rutvi Shah <rutvishah859@gmail.com>
2022-03-20 01:00:14 -0400

    Generates the sudoku board array from Lab3 puzzle.txt file

--- sudoku_sol_validator.c
+++ sudoku_sol_validator.c
@@ -3,7 +3,6 @@
 #include <pthread.h> 
 #include <semaphore.h> 
 
-
 /* structure for passing data to threads */ 
 typedef struct  
 {  
@@ -18,9 +17,7 @@ int validGrid[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 // check each column of the sudoku board, validate of all the values in the column are unique [1, 9]
 void *checkColumn(void *param) {
-  parameters *data = (parameters *) malloc(sizeof(parameters));
-  data->row = 1;
-  data->column = 1;  
+  
 }
 
 // check each row of the sudoku board, validate of all the values in the row are unique [1, 9]
@@ -34,5 +31,34 @@ void checkGrid(void *param) {
 }
 
 int main(void) { 
-  
+  // how to make param struct 
+  // parameters *data = (parameters *) malloc(sizeof(parameters));
+  // data->row = 1;
+  // data->column = 1; 
+
+  int board[9][9];
+
+  // open given arg file and read the file to extract the commands to run
+	FILE *fptr;
+	fptr = fopen("Lab3 puzzle.txt", "r");
+
+  // file error
+	if (fptr == NULL) {
+		printf("Couldn't open file\n");
+    exit(0);
+	}
+
+  // make a 9x9 sudoku array from the Lab3 puzzle.txt file
+  int i = 0;
+  int j = 0;
+  while (fscanf(fptr, "%d", &board[i][j]) == 1){
+    if (j == 8) {
+      j = 0;
+      i++;
+      continue;
+    }
+    j++;
+  }
+
+
 } 
