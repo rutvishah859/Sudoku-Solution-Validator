777c0928c541761506c3eb7c9f158fb17916db01
Rutvi Shah <rutvishah859@gmail.com>
2022-03-20 01:28:07 -0400

    Made printBoard function used to print a formatted sudoku board
    Co-Authored-By: Foram <24356597+foramgandhi12@users.noreply.github.com>

--- sudoku_sol_validator.c
+++ sudoku_sol_validator.c
@@ -10,6 +10,8 @@ typedef struct
     int column;  
 } parameters;
 
+int board[9][9];
+
 // if ith value is 1 then the region is valid, otherwise if 0 then it's invalid
 int validCol[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 int validRow[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
@@ -30,13 +32,30 @@ void checkGrid(void *param) {
 
 }
 
+// prints formatted sukoku board
+void printBoard () {
+  for (int i = 0; i  < 9; i++) {
+    for (int j = 0; j < 9; j++){
+      printf("%d ", board[i][j]);
+
+      if (j == 2 || j == 5) {
+        printf("| ");
+      }
+      if (j == 8) {
+        printf("\n");
+      }
+    }
+    if (i == 2 || i == 5) {
+      printf("------------------\n");
+    }
+  }
+}
+
 int main(void) { 
   // how to make param struct 
   // parameters *data = (parameters *) malloc(sizeof(parameters));
   // data->row = 1;
   // data->column = 1; 
-
-  int board[9][9];
 
   // open given arg file and read the file to extract the commands to run
 	FILE *fptr;
@@ -60,5 +79,8 @@ int main(void) {
     j++;
   }
 
-
+  // get puzzle solver inputs
+  // int puzzleRow; 
+  // int puzzleCol;
+  printBoard();
 } 
